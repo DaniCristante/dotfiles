769c7e732c9601d7117d22afa2861275f0c18dca
Daniel Cristante <daniel.cristante@gmail.com>
2022-04-17 01:20:50 +0200

    fix symlinking script

--- symlinks.bash
+++ symlinks.bash
@@ -12,12 +12,14 @@ echo "setting up symlinks..."
 for file in "${filesToLink[@]}"; do
   sourceFile="$PWD/$file"
   destinationFile="$HOME/$file"
-  if [ ! -f $file ]; then
-    ln -s $sourcefile $destinationFile
+  if [ ! -e $destinationFile ]; then
+    ln -fs $sourceFile $destinationFile
   else
     read -p "File $file already exists, do you want to OVERRIDE it?" -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]; then
+      echo
       echo "Overriding $file in $HOME"
+      rm -rf $destinationFile
       ln -fs $sourceFile $destinationFile
     else
       echo "Skipping symlinking of $file"
@@ -25,6 +27,3 @@ for file in "${filesToLink[@]}"; do
     fi
   fi
 done
-
-echo "Files successfully linked"
-
